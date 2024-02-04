08d13fe3b0ca01647f0c97e93b39e954fd54182d
Alex <alex@Aleksandrs-MacBook-Pro-2.local>
2024-02-04 23:56:10 +0200

    update: use random alert

--- apps/go/productivity_tool/alert_generator/main.go
+++ apps/go/productivity_tool/alert_generator/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/avilikof/monorepo/lib/go/alert_entity"
 )
 
 func main() {
 	println("Hello World")
-	alert := alert_entity.DefaultAlert()
-	println(alert)
+	alert := alert_entity.RandomAlert()
+	fmt.Printf("%+v", alert)
 }
