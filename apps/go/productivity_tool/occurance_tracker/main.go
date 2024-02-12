386ab6f7e51cdaa5640bb2724854ce6378716c93
Alex <alex@Aleksandrs-MacBook-Pro-2.local>
2024-02-12 23:53:43 +0200

    feat: add occurance_tracker

---
+++ apps/go/productivity_tool/occurance_tracker/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/avilikof/monorepo/lib/go/env_var"
+	"github.com/avilikof/monorepo/lib/go/kafka_driver"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello there")
+	readAlerts()
+}
+
+func readAlerts() {
+	envVars, err := env_var.NewEnvVarHandler()
+	if err != nil {
+		log.Println(err)
+	}
+	err = envVars.LoadDotEnv(".env")
+	if err != nil {
+		log.Printf("failed to load config: %v\n", err)
+		os.Exit(0)
+	}
+	kafkaClient := kafka_driver.NewKafkaHandler(&envVars)
+	err = kafkaClient.Subscribe(&envVars)
+	if err != nil {
+		log.Println(err)
+		os.Exit(0)
+	}
+	for {
+		err := printMessage(&kafkaClient)
+		if err != nil {
+			continue
+		}
+		//time.Sleep(1 * time.Second)
+	}
+}
+
+func printMessage(kafka *kafka_driver.KafkaHandler) error {
+	msg, err := kafka.Get()
+	if err != nil {
+		return err
+	}
+	if len(string(msg.Value)) == 0 {
+		return fmt.Errorf("empty response")
+	}
+	log.Println(string(msg.Value))
+	return nil
+}
