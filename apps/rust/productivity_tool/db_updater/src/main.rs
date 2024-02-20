18ae926a876f13e848ecdfc7af868e2ffc32e47e
Alex <alex@Aleksandrs-MacBook-Pro-2.local>
2024-02-20 22:44:47 +0200

    update: driver and updater

--- apps/rust/productivity_tool/db_updater/src/main.rs
+++ apps/rust/productivity_tool/db_updater/src/main.rs
@@ -8,12 +8,6 @@ use event_entity::EventEntity;
 use kafka_driver::{KafkaClientConfig, KafkaConsumerClient};
 use std::env;
 
-#[derive(Serialize, Debug, Deserialize)]
-struct MyDocument {
-    title: String,
-    body: String,
-}
-
 #[derive(Deserialize, Debug)]
 struct SearchResult<T> {
     hits: Hits<T>,
@@ -28,52 +22,33 @@ struct Hits<T> {
 struct Hit<T> {
     _source: T,
 }
-impl MyDocument {
-    pub fn new() -> Self {
-        Self {
-            body: "this is body".to_string(),
-            title: "this is title".to_string(),
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
     load(".env").expect("failed to load .env file");
     let url = env::var("ELASTIC_FULL_URL").unwrap();
-    let config = set_kafka_config();
+    let config = get_kafka_config();
     let kafka_client = create_kafka_client(&config);
     let es_client = ElasticClient::new(&url);
     loop {
         kafka_client.subscribe(&["event"]);
         match kafka_client.get_message().await {
             None => {}
-            Some(msg) => match EventEntity::from_bytes(&msg) {
-                Ok(event) => es_client.index("test", &event).await.expect("Do something"),
-                Err(_) => {}
-            },
+            Some(msg) => {
+                if let Ok(mut event) = EventEntity::from_bytes(&msg) {
+                    // es_client.index("test", &event).await.expect("Do something")
+                    es_client
+                        .index_as_byte("test", event.as_bytes().unwrap())
+                        .await
+                        .expect("something bad");
+                }
+            }
         }
     }
-    // let client = Client::new();
-    // let resp = client.get(&url).send().await;
-    // if let Ok(r) = resp {
-    //     println!("{:?}", r);
-    //     println!("{:?}", r.text().await);
-    // }
-    // match index_document(&client, "test", &MyDocument::new(), &url).await {
-    //     Ok(_) => {}
-    //     Err(err) => println!("{err}"),
-    // }
-    // match search_documents(&client, "test", "*", &url).await {
-    //     Ok(docs) => {
-    //         println!("{:?}", docs);
-    //     }
-    //     Err(err) => println!("{:?}", err),
-    // }
 }
 
-fn set_kafka_config() -> KafkaClientConfig {
+fn get_kafka_config() -> KafkaClientConfig {
     let bootstrap = env::var("KAFKA_URL").unwrap();
     let user = env::var("KAFKA_USER").unwrap();
     let pass = env::var("KAFKA_PASS").unwrap();
@@ -83,49 +58,4 @@ fn set_kafka_config() -> KafkaClientConfig {
 
 fn create_kafka_client(config: &KafkaClientConfig) -> KafkaConsumerClient {
     KafkaConsumerClient::connect(config)
-}
-async fn index_document(
-    client: &Client,
-    index: &str,
-    document: &MyDocument,
-    base_url: &str,
-) -> Result<(), reqwest::Error> {
-    let url = format!("{}/{}/_doc", base_url, index);
-    client
-        .post(url)
-        .json(document)
-        .send()
-        .await?
-        .error_for_status()?; // Checks if the response status is an error.
-    Ok(())
-}
-
-async fn search_documents(
-    client: &Client,
-    index: &str,
-    query: &str,
-    base_url: &str,
-) -> Result<Vec<MyDocument>, reqwest::Error> {
-    let url = format!("{}/{}/_search", base_url, index);
-    let response = client
-        .post(url)
-        .json(&serde_json::json!({
-            "query": {
-                "query_string": {
-                    "query": query
-                }
-            }
-        }))
-        .send()
-        .await?
-        .error_for_status()?
-        .json::<SearchResult<MyDocument>>()
-        .await?;
-
-    Ok(response
-        .hits
-        .hits
-        .into_iter()
-        .map(|hit| hit._source)
-        .collect())
 }
--- libs/rust/es-driver/src/lib.rs
+++ libs/rust/es-driver/src/lib.rs
@@ -1,5 +1,6 @@
 use reqwest::{Client, Error, Response};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 pub struct ElasticClient {
     client: Client,
@@ -28,11 +29,39 @@ impl ElasticClient {
             url: url.to_string(),
         }
     }
-    pub async fn index<B: Serialize>(&self, index: &str, doc: &B) -> Result<(), Error> {
-        let full_url = format!("{}/{}/_doc", self.url, index);
-        match self.client.post(full_url).json(doc).send().await {
-            Ok(_) => Ok(()),
-            Err(err) => Err(err),
-        }
+    pub async fn index(&self, index: &str, doc: Vec<u8>) -> Result<Response, Error> {
+        let ful_url = format!("{}/{}/_doc", self.url, index);
+        self.client
+            .post(ful_url)
+            .header("Content-Type", "application/json")
+            .body(doc)
+            .send()
+            .await
+    }
+
+    pub async fn search(&self, index: &str, query: &str) -> Result<Vec<Value>, reqwest::Error> {
+        let url = format!("{}/{}/_search", self.url, index);
+        let response = self
+            .client
+            .post(url)
+            .json(&serde_json::json!({
+                "query": {
+                    "query_string": {
+                        "query": query
+                    }
+                }
+            }))
+            .send()
+            .await?
+            .error_for_status()?
+            .json::<SearchResult<Value>>()
+            .await?;
+
+        Ok(response
+            .hits
+            .hits
+            .into_iter()
+            .map(|hit| hit._source)
+            .collect())
     }
 }
